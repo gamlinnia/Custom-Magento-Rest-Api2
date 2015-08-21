bd7b53113038f258247a3deea335ec2fddfdba4f
gamlinnia <gamlinnia@hotmail.com>
2015-08-21 14:41:25 +0800

    Update V1.php

--- app/code/local/Custom/Restapi/Model/Api2/Restapi/Rest/Admin/V1.php
+++ app/code/local/Custom/Restapi/Model/Api2/Restapi/Rest/Admin/V1.php
@@ -1,1 +1,53 @@
 <?php
+class Custom_Restapi_Model_Api2_Restapi_Rest_Admin_V1 extends Custom_Restapi_Model_Api2_Restapi
+{
+
+    public function _create(array $data) {
+
+        $firstName = $data['firstname'];
+        $lastName = $data['lastname'];
+        $email = $data['email'];
+        $password = $data['password'];
+
+        $customer = Mage::getModel("customer/customer");
+
+        $customer->setFirstname($firstName);
+        $customer->setLastname($lastName);
+        $customer->setEmail($email);
+        $customer->setPasswordHash(md5($password));
+        $customer->save();
+
+        return $this->_getLocation($customer);
+    }
+
+    public function _retrieve() {
+        //        in api2.xml <route>/customer/:id</route>
+        $id = (int)$this->getRequest()->getParam('id');
+        return json_encode(array("testing",$id));
+    }
+
+    public function _retrieveCollection() {
+        $configValue = Mage::getStoreConfig('sectionName/groupName/fieldName');
+        var_dump($configValue);
+//        return json_encode(array(
+//            'result' =>array("testing" => "Success", "good" => "no good")
+//        ));
+    }
+
+    public function _update() {
+
+    }
+
+    public function _multiUpdate() {
+
+    }
+
+    public function _delete() {
+
+    }
+
+    public function _multiDelete() {
+
+    }
+
+}
